2ce2e9663c0b4c6e0381181f36102f6af0d99519
andrei <andrei@outlook.com>
2024-01-07 16:28:38 -0300

    feat: login router import

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { router } from './routes/users.route';
+import { loginRouter } from './routes/login.route';
 
 const app = express();
 app.use(express.json())
+
 app.use(router);
+app.use(loginRouter)
 
 export {app}
