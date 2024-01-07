408ce7fc8e64d601a0161215fcbbf2b25e30c2a9
andrei <andrei@outlook.com>
2024-01-07 16:14:24 -0300

    feat: password client hash

--- src/core/useCases/createUser/createUser.useCase.ts
+++ src/core/useCases/createUser/createUser.useCase.ts
@@ -1,11 +1,13 @@
 import { User } from "../../../entities/user";
 import { ICreateUser } from "./interfaces/ICreateUser";
 import { IUsersRepository } from "./interfaces/repositories/IUsersRepository";
-
+import bcrypt from 'bcrypt'
 export class CreateUserUseCase {
     constructor(private usersRepository: IUsersRepository) { }
 
     async execute(user: ICreateUser) {
+
+        user.password = await bcrypt.hash(user.password, 4);
 
         const newUser = new User({
             ...user
