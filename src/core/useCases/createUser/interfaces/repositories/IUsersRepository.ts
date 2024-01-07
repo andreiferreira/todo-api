d494109f1f2793b943b569e11667233d4dc7a2f7
andrei <andrei@outlook.com>
2024-01-07 16:17:05 -0300

    feat: find user repository methods

--- src/core/useCases/createUser/interfaces/repositories/IUsersRepository.ts
+++ src/core/useCases/createUser/interfaces/repositories/IUsersRepository.ts
@@ -1,5 +1,8 @@
+import { IUser } from "../../../../../entities/user";
 import { ICreateUser } from "../ICreateUser";
 
 export interface IUsersRepository {
     create(user: ICreateUser): Promise<any>;
+    findByEmail(email: string): Promise<IUser>;
+    findById(id: string): Promise<IUser>;
 }
--- src/infra/repositories/usersRepository.ts
+++ src/infra/repositories/usersRepository.ts
@@ -1,13 +1,34 @@
 import { User } from '@prisma/client'
 import prismaClient from '../../../prisma/prismaClient'
-export class UsersRepository {
+import { IUsersRepository } from '../../core/useCases/createUser/interfaces/repositories/IUsersRepository'
+export class UsersRepository implements IUsersRepository {
 
     async create(user: User): Promise<any> {
 
-       return await prismaClient.user.create({
+        return await prismaClient.user.create({
             data: {
                 ...user
             }
         })
     }
- }
+
+    async findByEmail(email: string): Promise<User> {
+        return await prismaClient.user.findFirst(
+            {
+                where: {
+                    email: email
+                }
+            }
+        )
+    }
+
+    async findById(id: string): Promise<User> {
+        return await prismaClient.user.findUnique(
+            {
+                where: {
+                    id: id
+                }
+            }
+        )
+    }
+}
