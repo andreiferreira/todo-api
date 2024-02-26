21b16cb52c0578a06f39a6f9f7a0aed3fbfbfdbb
andrei <andrei@outlook.com>
2024-02-26 18:03:49 -0300

    feat: create new methods in tasksRepository

--- src/domain/interfaces/repositories/ITasksRepository.ts
+++ src/domain/interfaces/repositories/ITasksRepository.ts
@@ -1,6 +1,10 @@
 import { ITask } from "../../../entities/task";
+import { IUpdateTask } from "../models/IUpdateTask";
 
 export interface ITasksRepository {
     create(task: ITask): Promise<any>
     listTasksByUserId(userId: string): Promise<ITask[]>
+    getTask(taskId: string): Promise<ITask>
+    updateTask(task: IUpdateTask): Promise<ITask>
+    deleteTask(taskId: string): Promise<void>
 }
